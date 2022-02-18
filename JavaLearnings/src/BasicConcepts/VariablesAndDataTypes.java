72cc5f7e83076e4f7efec50888be5228d86092c1
pavangoundla <pavangoundla0707@gmail.com>
2022-02-18 12:21:57 +0530

    Added variables and Data types

---
+++ JavaLearnings/src/BasicConcepts/VariablesAndDataTypes.java
@@ -0,0 +1,37 @@
+package BasicConcepts;
+
+public class VariablesAndDataTypes {
+	
+	/* NOTES:
+	 * 
+	 * VARIABLES AND DATA TYPES:
+	 * ========================
+	 * 
+	 * TECH DEF: 
+	 * 1. In java,  variable is a name given to memory location and this variable is associated with a value
+	 * 2. In Java, Data type specifies the size and type of values
+	 * 
+	 * SYNTAX: DataType VariableName = Value;
+	 * 
+	 * REAL TIME: A tank full of milk
+	 * 
+	 * CONVERSION: milk heritageMilk = 50;
+	 * 
+	 * OTHER DETAILS:
+	 * 
+	 * variable and string should be separated with a concatenation operator i.e, + 
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int number = 99;
+		String name = "Pavan";
+		double dec = 2.2;
+		char letter = 'P';
+		boolean validate = true;
+		System.out.println(number + name + dec + letter + validate);
+	}
+
+}
